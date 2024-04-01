341c7d94e63af6546d52f04c3946227181deac9b
Pedro Lucas <pl.lucassouza@gmail.com>
2024-04-01 14:51:52 -0300

    Finishing the modal part of the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-const openModalButton = document.querySelector(".btn_enter");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
+const openModalButton = document.querySelector('.btn_enter');
+const closeModalButton = document.querySelector('.btn_close');
+const modal = document.querySelector('#modal');
+const main = document.querySelector('main');
+const header = document.querySelector('header');
 
-const toggleModal = ()=>{
-  modal.classList.toggle("hide");
-};
+const toggleModal = () => {
+  modal.classList.toggle('hide');
+  main.classList.toggle('opacity');
+  header.classList.toggle('opacity');
+}
 
-[openModalButton, fade].forEach((el)=>{
-  el.addEventListener('click', ()=> toggleModal());
-})
+[closeModalButton, openModalButton].forEach(el => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleModal();
+  })
+});
