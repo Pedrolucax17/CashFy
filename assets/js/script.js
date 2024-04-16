8484ba918230100dae39830417b465a8cd7281be
Pedro Lucas <pl.lucassouza@gmail.com>
2024-04-16 19:49:59 -0300

    updating the dashboard page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 const openModalButton = document.querySelector('.btn_enter');
 const closeModalButton = document.querySelector('.btn_close');
+const openModalButtonDashboard = document.querySelector('.btn_add');
+const closeModalButtonDashboard = document.querySelector('.btn_close');
 const modal = document.querySelector('#modal');
 const main = document.querySelector('main');
 const header = document.querySelector('header');
+const add = document.querySelector('.add');
+
+console.log(add);
 
 const toggleModal = () => {
   modal.classList.toggle('hide');
@@ -10,9 +15,14 @@ const toggleModal = () => {
   header.classList.toggle('opacity');
 }
 
-[closeModalButton, openModalButton].forEach(el => {
-  el.addEventListener('click', (e) => {
-    e.preventDefault();
-    toggleModal();
-  })
+// [closeModalButton, openModalButton].forEach(el => {
+//   el.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     toggleModal();
+//   })
+// });
+
+openModalButtonDashboard.addEventListener('click', ()=> {
+  toggleModal();
 });
+
