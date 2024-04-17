bf58d605718e0e6f32a4fc9b750dc0a6d285523c
Pedro Lucas <pl.lucassouza@gmail.com>
2024-04-17 19:57:19 -0300

    finalizing a dashboard page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,25 @@
 const openModalButton = document.querySelector('.btn_enter');
 const closeModalButton = document.querySelector('.btn_close');
 const openModalButtonDashboard = document.querySelector('.btn_add');
-const closeModalButtonDashboard = document.querySelector('.btn_close');
+const closeModalButtonDashboard = document.querySelector('.btn_close_dashboard');
 const modal = document.querySelector('#modal');
+const modalDashboard = document.querySelector('#modal_dashboard');
 const main = document.querySelector('main');
 const header = document.querySelector('header');
 const add = document.querySelector('.add');
 
-console.log(add);
-
 const toggleModal = () => {
-  modal.classList.toggle('hide');
+  modal?.classList.toggle('hide');
   main.classList.toggle('opacity');
   header.classList.toggle('opacity');
+  modalDashboard?.classList.toggle('hide');
 }
 
-// [closeModalButton, openModalButton].forEach(el => {
-//   el.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     toggleModal();
-//   })
-// });
 
-openModalButtonDashboard.addEventListener('click', ()=> {
-  toggleModal();
+[closeModalButton, openModalButton, add, closeModalButtonDashboard].forEach(el => {
+  el?.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleModal();
+  })
 });
 
